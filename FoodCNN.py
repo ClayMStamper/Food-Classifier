d732b33358577cf0f7b1cfdb0c5a7327bc1330f5
clayton <claystamper@gmail.com>
2019-11-26 23:17:46 -0600

    mend

--- FoodCNN.py
+++ FoodCNN.py
@@ -5,6 +5,8 @@ from keras.layers import Flatten
 from keras.layers import Dense
 import tensorflow as tf
 from keras_preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+from keras.utils import plot_model
 
 cnn = Sequential()
 
@@ -29,27 +31,36 @@ cnn.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accurac
 
 # image augmentation - prevent overfitting
 train_datagen = ImageDataGenerator(
-        rescale=1./255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
 
-test_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_set = train_datagen.flow_from_directory(
-        '../train',
-        target_size=(64, 64),
-        batch_size=32)
+    '../train',
+    target_size=(64, 64),
+    batch_size=32)
 
 test_set = test_datagen.flow_from_directory(
-        '../test',
-        target_size=(64, 64),
-        batch_size=32)
+    '../test',
+    target_size=(64, 64),
+    batch_size=32)
 
 cnn.fit_generator(
-        train_set,
-        steps_per_epoch=8000, # we have 8k images in our training set
-        epochs=1,
-        validation_data=test_set,
-        validation_steps=2000)
+    train_set,
+    steps_per_epoch=80,  # we have 8k images in our training set
+    epochs=1,
+    validation_data=test_set,
+    validation_steps=20)
 
+# visualize training set results
+plot_model(cnn, to_file='model.png')
+# plt.scatter(cnn.metrics[0][:, 50], cnn.metrics[0][:, 50], color='red')
+# # plt.plot(X_train, regressor.predict(X_train), color='blue')
+# plt.plot(cnn.metrics[0]. cnn.metrics[1], color = 'red')
+# plt.title('Loss vs Accuracy')
+# plt.xlabel(cnn.metrics_names[0])
+# plt.ylabel(cnn.metrics_names[1])
+# plt.show()
